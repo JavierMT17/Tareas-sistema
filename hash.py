05d79b465ed2539691c28edb24a9f3d4d0b62966
JavierMT <jmorillot@iesrafaelalberti.es>
2025-11-17 09:14:41 +0100

    Add files via upload

---
+++ hash.py
@@ -0,0 +1,19 @@
+import time
+import hashlib
+import itertools
+import sys
+
+def descifrar_hash()
+
+
+
+
+
+
+
+def main():
+    inicio = time.time()
+    mayusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","X","Y","Z"]
+    minusculas =["a","b","c","d","e", "f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","x","y","z"]
+    numeros = [0,1,2,3,4,5,6,7,8,9,]
+    caracteres = ["!","@","#","$","%","&","/","(",")","="]
