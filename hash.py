6424ce418423af3ae8f47757458853c14bdcd444
JavierMT <jmorillot@iesrafaelalberti.es>
2025-11-17 21:46:04 +0100

    Update hash.py

--- hash.py
+++ hash.py
@@ -4,16 +4,16 @@ import itertools
 import sys
 
 
-palabra= "PEPE"
+
+palabra= sys.argv[1]
 palabra_normal = palabra.encode()
 hash_generado = hashlib.sha256(palabra_normal).hexdigest()
 print(hash_generado)
 
 
-
 inicio = time.time()
-mayusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","Q","R","S","T","U","V","W","X","Y","Z"]
-minusculas =["a","b","c","d","e", "f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","x","y","z"]
+mayusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+minusculas =["a","b","c","d","e", "f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"]
 numeros = ["0","1","2","3","4","5","6","7","8","9"]
 caracteres = ["!","@","#","$","%","&","/","(",")","="]
 
